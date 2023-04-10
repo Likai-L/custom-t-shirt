14604793a1d92b9f220cbd0de0a89db4a5f35685
Likai Liu <likailiu2@gmail.com>
2023-04-10 15:41:09 +0800

    Canvas set up

---
+++ client/src/canvas/CanvasModel.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Environment, Center } from "@react-three/drei";
+
+import CameraRig from "./CameraRig";
+import Backdrop from "./Backdrop";
+import Shirt from "./Shirt";
+
+export default function CanvasModel() {
+  return (
+    <Canvas>
+      <ambientLight intensity={0.5} />
+      <Environment preset="city" />
+      <CameraRig>
+        {/* <Backdrop> */}
+        <Center>
+          <Shirt />
+        </Center>
+        {/* </Backdrop> */}
+      </CameraRig>
+    </Canvas>
+  );
+}
