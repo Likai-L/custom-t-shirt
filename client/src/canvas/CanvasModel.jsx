b0312e9104cdb8ab3e665bbb75b5e3a168940ffd
Likai Liu <likailiu2@gmail.com>
2023-04-12 17:35:43 +0800

    Turn on the backdrop and change the camera fov

--- client/src/canvas/CanvasModel.jsx
+++ client/src/canvas/CanvasModel.jsx
@@ -8,15 +8,19 @@ import Shirt from "./Shirt";
 
 export default function CanvasModel() {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{ position: [0, 0, 0], fov: 30 }}
+      gl={{ preserveDrawingBuffer: true }}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
       <CameraRig>
-        {/* <Backdrop> */}
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
-        {/* </Backdrop> */}
       </CameraRig>
     </Canvas>
   );
