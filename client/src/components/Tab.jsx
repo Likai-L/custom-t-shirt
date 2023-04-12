060946523ce9ae09b1b890a6a8b128bf21d1ec62
Likai Liu <likailiu2@gmail.com>
2023-04-12 21:21:53 +0800

    Add the Tab component

---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+export default function Tab({ isFilterTab, isActiveTab, handleClick, tab }) {
+  const snap = useSnapshot(state);
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? "rounded-full glassmorhism" : "rounded-4"
+      }`}
+      onClick={handleClick}
+      style={activeStyles}
+    >
+      <img
+        src={tab.icon}
+        alt={tab.name}
+        className={`${isFilterTab ? "" : "w-[80%] h-[80%]"}`}
+      />
+    </div>
+  );
+}
