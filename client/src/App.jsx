816f9787384823fc1cfbdb6289f2fc72c3130539
Likai Liu <likailiu2@gmail.com>
2023-04-12 16:31:46 +0800

    Fix component name

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./index.css";
 import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
-import Canvas from "./canvas/Canvas";
+import CanvasModel from "./canvas/CanvasModel";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,7 +10,7 @@ function App() {
   return (
     <main className="app transition-all">
       <Home></Home>
-      <Canvas></Canvas>
+      <CanvasModel></CanvasModel>
       <Customizer></Customizer>
     </main>
   );
