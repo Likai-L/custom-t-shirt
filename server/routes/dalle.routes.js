ec6bff0d4c21d8a34e64e4a78cb722f7e36dce00
Likai Liu <likailiu2@gmail.com>
2023-04-13 20:31:43 +0800

    Implement the image generation api route

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -5,9 +5,38 @@ import { Configuration, OpenAIApi } from "openai";
 dotenv.config();
 
 const router = express.Router();
+const config = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openAi = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
-  res.status(200).json({ message: "this is /api/v1/dalle" });
+  res.status(200).json({ message: "Hello from DALL.E routes!" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openAi.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = response.data.data[0].b64_json;
+    res.status(200).json({ image });
+  } catch (error) {
+    console.log("Error during DALL.E request", error);
+    res
+      .status(500)
+      .json({
+        message:
+          "There was an error when trying to generate an image with Dall.E",
+      });
+  }
 });
 
 export default router;
