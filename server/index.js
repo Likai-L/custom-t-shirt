06e5001eedb8e4541fb7c9f3090a9f6867ae7b34
Likai Liu <likailiu2@gmail.com>
2023-04-13 17:34:34 +0800

    Add routes

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import dalleRoutes from "./routes/dalle.routes.js";
+
 dotenv.config();
 
 const app = express();
+
+// middlewares
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello From Dall-E!" });
